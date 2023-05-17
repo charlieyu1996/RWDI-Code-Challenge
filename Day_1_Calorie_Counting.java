6f734da919dc1f2334dd57081fdd8a217843fbc3
Charlie Yu <charlieyu1996@gmail.com>
2023-05-17 12:47:14 -0400

    part 1 day 2

--- Day_1_Calorie_Counting.java
+++ Day_1_Calorie_Counting.java
@@ -3,25 +3,41 @@ import java.util.ArrayList;
 import java.util.PriorityQueue;
 
 class Day_1_Calorie_Counting {
-    public static int getMostCalories(ArrayList<ArrayList<Integer>> elfCalories){
+
+    // returns the top N calories from the list of calories
+    public static int getMostCalories(ArrayList<ArrayList<Integer>> elfCalories, int topN){
         int currMax = 0;
-        // Create a PriorityQueue that sorts in Descending order
-        // PriorityQueue<Integer> pq = new PriorityQueue<>(3, (calorie1, calorie2) -> calorie1 >= calorie2 ? -1 : 1 ); 
+        // Create a PriorityQueue that sorts in ascending order
+        PriorityQueue<Integer> pq = new PriorityQueue<>((calorie1, calorie2) -> calorie1 >= calorie2 ? 1 : -1); 
 
         for (ArrayList<Integer> currElf : elfCalories){
             int total = 0;
             for (int currCalorie : currElf){
                 total += currCalorie;
             }
-            currMax = Math.max(currMax, total);
+
+            // offer the value to pq first, and remove the smallest number if over capacity
+            pq.offer(total);
+            if (pq.size() > topN){
+                pq.poll();
+            }
         }
+        
+        for (Integer i : pq){
+            System.out.println(i);
+            currMax += i;
+        }
+
         return currMax;
     }
 
     public static void main(String[] args){
         try {
-            // "Day_1_input.txt"
+            // example input: "Day_1_input.txt"
             String fileName = args[0];
+
+            // example input: 3
+            int topN = Integer.parseInt(args[1]);
 
             File file = new File(fileName);
             FileReader fr = new FileReader(file);
@@ -46,7 +62,7 @@ class Day_1_Calorie_Counting {
                 completeArray.add(currArray);
             }
 
-            System.out.println(getMostCalories(completeArray));
+            System.out.println(getMostCalories(completeArray, topN));
 
         } catch (FileNotFoundException e) {
             System.out.println("Exception: " + args[0] + " not found");
@@ -55,7 +71,6 @@ class Day_1_Calorie_Counting {
             e.printStackTrace();
         }
     }
-
 }
 
 
